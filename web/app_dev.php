b6d89aa675f46969f713e1cd70cc114287dec8b1
Loïc Chardonnet <loic.chardonnet@gmail.com>
2014-03-01 20:03:27 +0100

    [FixInstall] Removed bootstrap.php.cache

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,10 +3,10 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-$loader = require_once __DIR__.'/../app/bootstrap.php.cache';
-Debug::enable();
-
+require_once __DIR__.'/../app/autoload.php';
 require_once __DIR__.'/../app/AppKernel.php';
+
+Debug::enable();
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
