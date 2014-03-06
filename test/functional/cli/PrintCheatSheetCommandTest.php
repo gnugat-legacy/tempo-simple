d3beeeb49b31dd0a4567fd351c27da2ba9583256
Loïc Chardonnet <loic.chardonnet@gmail.com>
2014-03-06 08:04:33 +0100

    [Refactoring2] Used TimeOfDayFactory->now()
    Now the commands don't rely directly on PHP time functions.

--- test/functional/cli/PrintCheatSheetCommandTest.php
+++ test/functional/cli/PrintCheatSheetCommandTest.php
@@ -19,6 +19,7 @@ class PrintCheatSheetCommandTest extends CommandTestCase
 {
     const MONTH = '1989-01';
     const DAY = '1989-01-25';
+    const HOUR = '12:05';
 
     public function testExecute()
     {
@@ -35,6 +36,14 @@ class PrintCheatSheetCommandTest extends CommandTestCase
         $dateFactory = $this->prophet->prophesize($dateFactoryClass);
         $dateFactory->today()->willReturn($date->reveal());
 
+        $timeOfDayClass = 'TempoSimple\DomainModel\Time\TimeOfDay';
+        $timeOfDay = $this->prophet->prophesize($timeOfDayClass);
+        $timeOfDay->getHour()->willReturn(self::HOUR);
+
+        $timeOfDayFactoryClass = 'TempoSimple\Service\TimeBundle\Factory\TimeOfDayFactory';
+        $timeOfDayFactory = $this->prophet->prophesize($timeOfDayFactoryClass);
+        $timeOfDayFactory->now()->willReturn($timeOfDay->reveal());
+
         $timeCardRepositoryClass = 'TempoSimple\DataSource\DoctrineBundle\Entity\TimeCardRepository';
         $timeCardRepository = $this->prophet->prophesize($timeCardRepositoryClass);
 
@@ -43,6 +52,7 @@ class PrintCheatSheetCommandTest extends CommandTestCase
 
         $command = new PrintCheatSheetCommand(
             $dateFactory->reveal(),
+            $timeOfDayFactory->reveal(),
             $timeCardRepository->reveal(),
             $templating->reveal(),
             $defaultProject
